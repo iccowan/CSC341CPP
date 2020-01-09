6accd50abae38da5af0954b2bfb20153bb94a1ac
Ian Cowan <ian.cowan@centre.edu>
2020-01-08 19:40:19 -0500

    Begin numbers

---
+++ NumbersToOrder/NumbersToOrder.cc
@@ -0,0 +1,26 @@
+/*
+  Name: Ian Cowan
+  Lab Partner: Jack Pharies
+  CSC 341 W20
+  P1 Numbers to Order
+  Takes a series of input of numbers −32768 ≤ k ≤ 32767 and sorts the numbers.
+  It also checks to see if there are any repeats and prints either "REPEATS" or "NO REPEATS".
+
+  **Write compilation instructions here**
+ */
+
+#include <iostream>
+
+
+int main()
+{
+  int myArray[65535];
+  for(int i = 0; i < 65535; i++)
+  {
+    //if(i == 0 || i == 65534)
+    //{
+      std::cout << myArray[i] << std::endl;
+    //}
+  }
+  return 0;
+}
